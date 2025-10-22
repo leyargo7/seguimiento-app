bc4d24cd72474da0cdc5b5fd7e0d1fe1a85a1a4c
arley <leyargo@hotmail.com>
2025-10-22 17:38:08 -0500

    probando nuevamente añadiendo footer y corrigiendo error de rol

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div>
         Desarrollado por 
         <Link href="https://arley-dev.vercel.app" className='text-blue-500 hover:text-blue-700'>
-        <span> arley dev</span>
+        <span> arley-dev</span>
         
         </Link>
         <br />
